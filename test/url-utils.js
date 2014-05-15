aa185e4bccb77b624405a9833323d63d7834c347
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-15 18:14:09 +0200

    added more url tests

--- test/url-utils.js
+++ test/url-utils.js
@@ -5,10 +5,12 @@ var getResourceURL = require('../lib/url-utils').getResourceURL;
 describe('#url', function () {
   it('should return the absolute url up to the id of the resource', function () {
     expect(getResourceURL('http://test.fr/authors/1', 'authors')).to.eql('http://test.fr/authors/');
+    expect(getResourceURL('http://test.fr/authors/1/', 'authors')).to.eql('http://test.fr/authors/');
   });
 
-  it ('should return the same url if there is no resource id', function () {
+  it('should return the same url if there is no resource id', function () {
     expect(getResourceURL('http://test.fr/authors/', 'authors')).to.eql('http://test.fr/authors/');
+    expect(getResourceURL('http://test.fr/authors', 'authors')).to.eql('http://test.fr/authors/');
   });
 
   it('should return the absolute url up to the query of the resource', function () {
