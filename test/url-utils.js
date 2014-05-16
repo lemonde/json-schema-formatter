1c3e8aea7ac168df32b8607738c706f47e812fef
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-05-16 11:03:19 +0200

    cleaned url utils tests description

--- test/url-utils.js
+++ test/url-utils.js
@@ -2,19 +2,21 @@ var expect = require('chai').expect;
 
 var getResourceURL = require('../lib/url-utils').getResourceURL;
 
-describe('#url', function () {
-  it('should return the absolute url up to the id of the resource', function () {
-    expect(getResourceURL('http://test.fr/authors/1', 'authors')).to.eql('http://test.fr/authors');
-    expect(getResourceURL('http://test.fr/authors/1/', 'authors')).to.eql('http://test.fr/authors');
-  });
+describe('url utils', function () {
+  describe('#getResourceURL', function () {
+    it('should return the absolute url up to the id of the resource', function () {
+      expect(getResourceURL('http://test.fr/authors/1', 'authors')).to.eql('http://test.fr/authors');
+      expect(getResourceURL('http://test.fr/authors/1/', 'authors')).to.eql('http://test.fr/authors');
+    });
 
-  it('should return the same url if there is no resource id', function () {
-    expect(getResourceURL('http://test.fr/authors/', 'authors')).to.eql('http://test.fr/authors');
-    expect(getResourceURL('http://test.fr/authors', 'authors')).to.eql('http://test.fr/authors');
-  });
+    it('should return the same url if there is no resource id', function () {
+      expect(getResourceURL('http://test.fr/authors/', 'authors')).to.eql('http://test.fr/authors');
+      expect(getResourceURL('http://test.fr/authors', 'authors')).to.eql('http://test.fr/authors');
+    });
 
-  it('should return the absolute url up to the query of the resource', function () {
-    expect(getResourceURL('http://test.fr/authors/?name=test', 'authors')).to.eql('http://test.fr/authors');
-    expect(getResourceURL('http://test.fr/authors?name=test', 'authors')).to.eql('http://test.fr/authors');
+    it('should return the absolute url up to the query of the resource', function () {
+      expect(getResourceURL('http://test.fr/authors/?name=test', 'authors')).to.eql('http://test.fr/authors');
+      expect(getResourceURL('http://test.fr/authors?name=test', 'authors')).to.eql('http://test.fr/authors');
+    });
   });
 });
