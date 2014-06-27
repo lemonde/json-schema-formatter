ab09eef83d8d215696cc82a8d9f24b0b907c7086
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-06-27 14:43:48 +0200

    test(expand): test expand multiple resources

--- test/middleware.js
+++ test/middleware.js
@@ -207,4 +207,64 @@ describe('formatter middleware', function () {
     .expect(check)
     .end(done);
   });
+  it('should be able to expand multiple related resources', function (done) {
+    app.get('/resource', function (req, res, next) {
+      res.body = {
+        id: 1,
+        data: 'test',
+        authors: {
+          id: 1
+        },
+        test: {
+          id: 1
+        }
+      };
+      next();
+    });
+
+    app.get('/resource', formatMiddleware({
+      domain: 'http://test.fr',
+      name: 'resource',
+      withRelated: [
+        {
+        name: 'authors',
+        url: 'http://test.fr/authors/'
+        },
+        {
+          name: 'test',
+          url: 'http://test.fr/test/'
+        }
+      ]
+    }));
+
+    function check(res) {
+     expect(res.body).to.eql({
+        metas: {
+          self: {
+            href: 'http://test.fr/resource?expand=resource.authors,resource.test'
+          }
+        },
+        resource: [{
+          href: 'http://test.fr/resource/1',
+          id: 1,
+          data: 'test',
+          links: {
+            authors: [{
+              id: 1,
+              href: 'http://test.fr/authors/1'
+            }],
+            test: [{
+              id: 1,
+              href: 'http://test.fr/test/1'
+            }]
+          }
+        }]
+      });
+    }
+
+    request(app)
+    .get('/resource?expand=resource.authors,resource.test')
+    .expect(check)
+    .end(done);
+  });
 });
