29c3ffc0fec33fa1e03aa9a2ef9578887dac84c6
Yannick Dominguez <yannick.dominguez@gmail.com>
2014-07-17 15:22:34 +0200

    refactored publicly exposes modules

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 'use strict';
 
-module.exports = require('./lib/middleware');
+module.exports = {
+  format: require('./lib/formatter'),
+  parse: require('./lib/parser'),
+  formatMiddleware: require('./lib/format-middleware'),
+  parseMiddleware: require('./lib/parse-middleware')
+}
